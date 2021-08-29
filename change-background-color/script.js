da0014a2055d25af86766510ebed618899742b1f
mani yousefi <maniyousefi.dev@gmail.com>
2021-08-29 12:47:04 +0430

    29 aug 2021 - 4

--- change-background-color/script.js
+++ change-background-color/script.js
@@ -1,7 +1,4 @@
-function changeBackgroundColor() 
-{
-
+function changeBackgroundColor() {
   const color = document.getElementById('colorPicker').value;
-
   document.body.style.backgroundColor = color;
 }
